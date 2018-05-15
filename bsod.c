262b9fe38240dce193e14e39b3d9fe16ecb187f8
myzhang1029 <myzhang1029@163.com>
2018-05-15 18:03:34 +0800

    Use a thread for drawing

--- bsod.c
+++ bsod.c
@@ -19,6 +19,7 @@
  */
 
 #include <Windows.h>
+#include <process.h>
 #include <stdio.h>
 #include <time.h>
 #ifndef __GNUC__
@@ -28,7 +29,14 @@
 #endif
 int SetUpWindowClass(char *, int, int, int, HINSTANCE);
 LRESULT CALLBACK WindowProcedure(HWND, unsigned int, WPARAM, LPARAM);
-unsigned redrawcount;
+unsigned firstdraw = 1;
+intptr_t curthrd = 0;
+typedef struct
+{
+	HDC hDC;
+	int horipos;
+	int horiinc;
+} THRDPARAM;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance __UNUSED, LPSTR lpCmdLine __UNUSED,
 		   int nCmdShow __UNUSED)
@@ -37,7 +45,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance __UNUSED, LPSTR 
 	MSG uMsg;
 	if (!SetUpWindowClass("1", 0, 0, 128, hInstance)) /* Navy blue */
 		return 1;
-	redrawcount = 0;
 	hwnd = CreateWindow("1", 0, WS_BORDER, 0, 0, 100, 100, NULL, NULL, hInstance, NULL);
 	SetWindowLongPtr(hwnd, GWL_STYLE, 0);
 	if (!hwnd)
@@ -75,6 +82,77 @@ int SetUpWindowClass(char *Title, int bgRed, int bgGreen, int bgBlue, HINSTANCE 
 		return 0;
 }
 
+void __cdecl draw_proc(void *phdc)
+{
+	HWND hWnd = phdc;
+	char *Text[] = {"A problem has been detected and windows has been shut down to prevent damage",
+			"to your computer.",
+			"",
+			"A process or thread crucial to system operation has unexpectedly exited or been",
+			"terminated.",
+			"",
+			"If this is the first time you've seen this stop error screen,",
+			"restart your computer. If this screen appears again, follow these steps:",
+			"",
+			"Check to make sure any new hardware or software is properly installed.",
+			"If this is a new installation, ask your hardware or software manufacturer",
+			"for any Windows updates you might need.",
+			"",
+			"If problems continue, disable or remove any newly installed hardware",
+			"or software. Disable BIOS memory options such as caching or shadowing.",
+			"If you need to use Safe Mode to remove or disable components, restart",
+			"your computer, press F8 to select Advanced Startup "
+			"Options, and then",
+			"select Safe Mode.",
+			"",
+			"Technical information:",
+			"",
+			"*** STOP: 0x000000F4 (0x00000003,0x87839060,0x87839CCC,0x83E40020)",
+			"",
+			"",
+			"",
+			"Collecting data for crash dump ...",
+			"Initializing disk for crash dump ...",
+			"Beginning dump of physical memory.",
+			"Dumping physical memory to disk: 0",
+			NULL};
+	int horipos = 5, horiinc;
+	PAINTSTRUCT ps;
+	HDC hDC = BeginPaint(hWnd, &ps);
+	RECT winrect;
+	HFONT font;
+	GetWindowRect(hWnd, &winrect);
+	horiinc = winrect.bottom / 36;
+	font = CreateFont(winrect.bottom / 48, winrect.right / 84, 0, 0, 0, 0, 0, 0, ANSI_CHARSET, OUT_DEFAULT_PRECIS,
+			  CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, FF_DONTCARE, "consolas");
+	SetTextColor(hDC, RGB(192, 192, 192));
+	SetBkColor(hDC, RGB(0, 0, 128));
+	SelectObject(hDC, font);
+	for (int count = 0; Text[count] != NULL; ++count, horipos += horiinc)
+	{
+		TextOut(hDC, 5, horipos, Text[count], strlen(Text[count]));
+	}
+	if (firstdraw) /* Draw a increasing process if it's the first draw */
+	{
+		char dumpstatus[37] = {0};
+		for (int count = 5; count <= 100; count += 5)
+		{
+			clock_t t = clock(); /* Delay a second */
+			while (1)
+				if ((int)((clock() - t) / CLOCKS_PER_SEC) >= 1)
+					break;
+			snprintf(dumpstatus, 37, "Dumping physical memory to disk: %d", count);
+			TextOut(hDC, 5, horipos - horiinc, dumpstatus, strlen(dumpstatus));
+		}
+	}
+	TextOut(hDC, 5, horipos - horiinc, "Dumping physical memory to disk: 100", 37);
+	TextOut(hDC, 5, horipos, "Physical memory dump complete.", 30);
+	TextOut(hDC, 5, horipos + horiinc,
+		"Contact your system admin or technical support group for further assistance.", 76);
+	DeleteObject(font);
+	EndPaint(hWnd, &ps);
+	curthrd = 0;
+}
 LRESULT CALLBACK WindowProcedure(HWND hWnd, unsigned int uiMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uiMsg)
@@ -93,76 +171,9 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, unsigned int uiMsg, WPARAM wParam, L
 			SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOSENDCHANGING);
 			break;
 		case WM_PAINT:
-		{
-			char *Text[] = {
-			    "A problem has been detected and windows has been shut down to prevent damage",
-			    "to your computer.",
-			    "",
-			    "A process or thread crucial to system operation has unexpectedly exited or been",
-			    "terminated.",
-			    "",
-			    "If this is the first time you've seen this stop error screen,",
-			    "restart your computer. If this screen appears again, follow these steps:",
-			    "",
-			    "Check to make sure any new hardware or software is properly installed.",
-			    "If this is a new installation, ask your hardware or software manufacturer",
-			    "for any Windows updates you might need.",
-			    "",
-			    "If problems continue, disable or remove any newly installed hardware",
-			    "or software. Disable BIOS memory options such as caching or shadowing.",
-			    "If you need to use Safe Mode to remove or disable components, restart",
-			    "your computer, press F8 to select Advanced Startup "
-			    "Options, and then",
-			    "select Safe Mode.",
-			    "",
-			    "Technical information:",
-			    "",
-			    "*** STOP: 0x000000F4 (0x00000003,0x87839060,0x87839CCC,0x83E40020)",
-			    "",
-			    "",
-			    "",
-			    "Collecting data for crash dump ...",
-			    "Initializing disk for crash dump ...",
-			    "Beginning dump of physical memory.",
-			    "Dumping physical memory to disk: 0",
-			    NULL};
-			int horipos = 5, horiinc;
-			char dumpstatus[37] = {0};
-			PAINTSTRUCT ps;
-			HDC hDC = BeginPaint(hWnd, &ps);
-			RECT winrect;
-			HFONT font;
-			GetWindowRect(hWnd, &winrect);
-			horiinc = winrect.bottom / 36;
-			font = CreateFont(winrect.bottom / 48, winrect.right / 84, 0, 0, 0, 0, 0, 0, ANSI_CHARSET,
-					  OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, FF_DONTCARE,
-					  "consolas");
-			SetTextColor(hDC, RGB(192, 192, 192));
-			SetBkColor(hDC, RGB(0, 0, 128));
-			SelectObject(hDC, font);
-			for (int count = 0; Text[count] != NULL; ++count, horipos += horiinc)
-			{
-				TextOut(hDC, 5, horipos, Text[count], strlen(Text[count]));
-			}
-			if (redrawcount == 0)/* Draw a increasing process if it's the first draw */
-				for (int count = 5; count <= 100; count += 5)
-				{
-					clock_t t = clock(); /* Delay a second */
-					while (1)
-						if ((int)((clock() - t) / CLOCKS_PER_SEC) >= 1)
-							break;
-					snprintf(dumpstatus, 37, "Dumping physical memory to disk: %d", count);
-					TextOut(hDC, 5, horipos - horiinc, dumpstatus, strlen(dumpstatus));
-				}
-			else
-				TextOut(hDC, 5, horipos - horiinc, "Dumping physical memory to disk: 100", 37);
-			TextOut(hDC, 5, horipos, "Physical memory dump complete.", 30);
-			TextOut(hDC, 5, horipos + horiinc,
-				"Contact your system admin or technical support group for further assistance.", 76);
-			DeleteObject(font);
-			EndPaint(hWnd, &ps);
+			if (curthrd == 0)
+				curthrd = _beginthread(draw_proc, 0, hWnd);
 			break;
-		}
 		default:
 			return DefWindowProc(hWnd, uiMsg, wParam, lParam);
 	}
