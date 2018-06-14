786478b24be6a3c3abfd894ddbbbb79beddca47b
myzhang1029 <myzhang1029@163.com>
2018-06-14 16:06:12 +0800

    Prime3 which uses Sieve of Eratosthenes algorithm

---
+++ prime3.c
@@ -0,0 +1,48 @@
+/* Program to print prime numbers less than MAXPRIME with Sieve of Eratosthenes algorithm */
+/*
+ * prime3.c
+ * Copyright (C) 2018 Zhang Maiyun <myzhang1029@163.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXPRIME 10000000000UL
+
+int main(void)
+{
+	unsigned long i, j;
+	bool *primes = malloc(MAXPRIME);
+	if (primes == NULL)
+		exit((fprintf(stderr, "malloc failed\n"), 1));
+	for (i = 2; i < MAXPRIME; i++)
+		primes[i] = true;
+	primes[0] = primes[1] = false;
+
+#pragma omp for
+	for (i = 2; i < sqrt(MAXPRIME); i++)
+		for (j = i * i; j < MAXPRIME; j += i)
+			primes[j] = false;
+
+	for (i = 0; i < MAXPRIME; i++)
+		if (primes[i] == true)
+			printf("%lu\n", i);
+
+	free(primes);
+	return 0;
+}
