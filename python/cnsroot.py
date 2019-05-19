e9f30f9b268fae9f03e2056c8a9e825ab0e0f4d3
myzhang1029 <myzhang1029@163.com>
2019-05-19 23:18:36 +0800

    Add code

---
+++ python/cnsroot.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# Print numbers that their nth root, n_2th root,... are all an interger
+# like 4096, 262144, 729 for 2 and 3
+# 
+# 
+# cnsroot.py
+# Copyright (C) 2017 Zhang Maiyun <myzhang1029@163.com>
+# 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# 
+
+from getopt import gnu_getopt, GetoptError
+from sys import argv, exit
+from sbl import lcm
+
+multiple = 1
+bmax = 3
+pmax = 3
+def usage(returncode):
+    print("options:\n")
+    print("-b, --base-max arg: maximum of the base\n")
+    print("-p, --power-max arg: maximum of power/multiple\n")
+    print("-m, --add-multiple arg: add a multiple\n")
+    print("-h, --help: show this\n")
+    exit(int(returncode))
+
+try:
+    opts, args = gnu_getopt(argv[1:], "b:p:m:h",
+            ["base-max=", "power-max=", "add-multiple=", "help"])
+except GetoptError as err:
+    print(err)
+
+for o, a in opts:
+    if o in ("-b", "--base-max"):
+        bmax = long(a)
+    elif o in ("-p", "--power-max"):
+        pmax = long(a)
+    elif o in ("-m", "--add-multiple"):
+        multiple = lcm(multiple, long(a))
+    elif o in ("-h", "--help"):
+        usage(0)
+    else:
+        print("unknown option", o)
+        usage(1)
+
+for base in range(0, bmax+1):
+    for power in range(0, pmax+1):
+        print(base**(multiple * power));
---
+++ python/generation.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# One man has n children, his children have n-1 children each, they have n-2 children each, how many people are there
+# in the family after n generations?
+# the mother side not included
+# n specified in argv[1], defaults to 1
+# UPDATE: this program actually calculates OEIS:A000522
+# 
+# generation.py
+# Copyright (C) 2018 Zhang Maiyun <myzhang1029@163.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# 
+
+from sys import argv
+
+try:
+    men = long(argv[1])
+except IndexError:
+    men = 1
+
+count = men # count for generations
+total = 1 # People in total
+total += men
+while count - 1 != 0:
+    count -= 1
+    men *= count
+    total += men
+print(total);
