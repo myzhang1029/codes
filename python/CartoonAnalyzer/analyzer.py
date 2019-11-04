fa4821137eee0a1ec15068bbe81231a49153a61a
Zhang Maiyun <myzhang1029@163.com>
2019-11-04 20:26:54 +0800

    (CartoonAnalyzer)Finished

--- python/CartoonAnalyzer/analyzer.py
+++ python/CartoonAnalyzer/analyzer.py
@@ -25,7 +25,26 @@ from docx.oxml import parse_xml
 import sys
 import json
 import argparse
-from colornames import ColorNames
+
+
+def rgbFromStr(s):
+    # s starts with a #.
+    r, g, b = int(s[1:3], 16), int(s[3:5], 16), int(s[5:7], 16)
+    return r, g, b
+
+
+def findNearestColorName(RGB_tuple, Map):
+    R = RGB_tuple[0]
+    G = RGB_tuple[1]
+    B = RGB_tuple[2]
+    mindiff = None
+    for d in Map:
+        r, g, b = rgbFromStr(Map[d])
+        diff = abs(R - r)*256 + abs(G-g) * 256 + abs(B - b) * 256
+        if mindiff is None or diff < mindiff:
+            mindiff = diff
+            mincolorname = d
+    return mincolorname, Map[mincolorname]
 
 
 def readppt(address, numbering):
@@ -50,24 +69,51 @@ def readppt(address, numbering):
                 if nc == 0 or nc == 41:
                     continue
                 colorMap = {
-                    "W": "FFFFFF",
-                    "R": "FF0000",
-                    "B": "00B0F0",
-                    "O": "FF6600",
-                    "Y": "FFFF00",
-                    "K": "7C7C7C"
+                    "W": "#FFFFFF",
+                    "R": "#FF0000",
+                    "B": "#00B0F0",
+                    "O": "#FF6600",
+                    "Y": "#FFFF00",
+                    "K": "#7C7C7C"
                 }
                 # if there is annotation, use it
-                if cell.text:
-                    color = cell.text
-                    code = colorMap[color]
-                else:
-                    # fall back to grip reading, might fail
-                    try:
-                        color, code = ColorNames.findNearestColorName(
+                color_anno = cell.text
+                code_anno = colorMap[color_anno][1:] if color_anno else ''
+                # fall back to grip reading, might fail
+                try:
+                    if cell.fill.fore_color.type == 1:
+                        color, code = findNearestColorName(
                             cell.fill.fore_color.rgb, colorMap)
-                    except AttributeError:
-                        color, code = "unknown", "FFFFFF"
+                    elif cell.fill.fore_color.type == 2:
+                        # XXX: Read ppt/theme/theme1.xml
+                        themeMap = {
+                            13: "K",
+                            14: "W"
+                        }
+                        try:
+                            color = themeMap[cell.fill.fore_color.theme_color]
+                        except KeyError as e:
+                            print(
+                                f"KeyError at {i}/{nr}/{nc}", file=sys.stderr)
+                            raise
+                        code = colorMap[color]
+                    code = code[1:]
+                    if color != color_anno:
+                        choice = input(
+                            f"The annotation at {i}/{nr}/{nc}, which is `{color_anno}' disagrees with the color `{color},{code}' analyzed by the program. Press 1 to proceed with the annotation and 2 to go with the program, with the annotation corrected, or 3 to temporarily use the program result: ")
+                        if choice == '1':
+                            color = color_anno
+                            code = code_anno
+                        elif choice == '2':
+                            cell.text = color
+                        elif choice == '3':
+                            pass
+                        else:
+                            ppt.save(address)
+                            raise Exception
+                except AttributeError:
+                    # print(i,nr,nc,cell.fill.fore_color.theme_color)
+                    color, code = "unknown", "FFFFFF"
                 if 0 < nc < 21:  # [1,20], read the left title
                     label = 0
                 elif 20 < nc < 41:  # [21,40], read the right title
@@ -85,6 +131,7 @@ def readppt(address, numbering):
                     "coord": coord,
                     "index": f"{m}.{n}" if numbering == "m.8" else str(i+1)
                 })
+    ppt.save(address)
     return result, len(ppt.slides)
 
 
