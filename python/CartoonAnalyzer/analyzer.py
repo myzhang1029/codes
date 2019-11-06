df98a16e909a8e63e29dd2c149b58104b026d462
Zhang Maiyun <myzhang1029@163.com>
2019-11-07 06:54:25 +0800

    (CartoonAnalyzer) update to fit broader usage

--- python/CartoonAnalyzer/analyzer.py
+++ python/CartoonAnalyzer/analyzer.py
@@ -20,6 +20,7 @@
 """Analyze YUSS 2020 cartoon specs and make instruction tables."""
 import pptx
 import docx
+from docx.shared import Cm
 from docx.oxml.ns import nsdecls
 from docx.oxml import parse_xml
 import sys
@@ -60,9 +61,9 @@ def readppt(address, numbering):
         n = i % 8 + 1
         for shape in slide.shapes:
             if shape.shape_type == 19:
-                table = shape
+                table = shape.table
                 break
-        for nr, row in enumerate(table.table.rows):
+        for nr, row in enumerate(table.rows):
             if nr == 0 or nr == 27:
                 continue  # skip title bar
             for nc, cell in enumerate(row.cells):
@@ -135,74 +136,95 @@ def readppt(address, numbering):
     return result, len(ppt.slides)
 
 
-def gendoc(data, length, numbering, outaddr, only):
+def gendoc(data, length, numbering, outaddr, only, lyrics):
     """Generate a docx file containing the tables of instruction.
     data: the data from readppt()
     length: the number of slides
     numbering: the numbering scheme
     outaddr: output path
     only: list of the classes to be made
+    lyrics: the lyrics file or None if not needed
     returns nothing"""
     doc = docx.Document()
     doc.core_properties.author = "YUSS 2020 Cartoon Analyzer"
+    # 26 classes
     gen = only if only else range(1, 27)
+    if lyrics:
+        lyricslst = json.load(open(lyrics))
     print(f"Generating for classes: {gen}")
     for classn in gen:
         print(classn)
         for person in range(40):
+            # Same lookup strategy
             coord = 'A' if person < 20 else 'B'
             coord += str(person % 20 + 1)
-            table = doc.add_table(rows=9, cols=8)
+            # I merge two tables into one to save paper
+            # There is a empty column between two
+            if person % 2 == 0:
+                offset = 0
+                table = doc.add_table(rows=9, cols=9)
+            else:
+                offset = 4 + 1
+            # Add grid lines
             table.style = 'Table Grid'
             r = table.rows[0].cells
-            r[0].text = "班级"
-            r[1].text = str(classn)
-            r[2].text = "编码"
-            r[3].text = coord
-            r[4].text = "姓名"
-            r[5].merge(r[7])
-            # TODO: numbering and n
-            for m in range(1, 5):
-                for n in range(1, 9):
-                    index = f"{m}.{n}"
-                    cell = None
-                    if m == 4 and n > 6:
+            r[offset + 0].text = "班级"
+            r[offset + 1].text = str(classn)
+            r[offset + 2].text = "编码"
+            r[offset + 3].text = coord
+            r = table.rows[1].cells
+            r[offset + 0].text = "姓名"
+            r[offset + 1].merge(r[offset + 3])
+            # Change if needed(XXX)
+            # effectiverows = nrows - the number of classs/name lines
+            metalines = 2
+            er = 9 - metalines
+            for i in range(length):
+                # Only for m.n numbering
+                m = i // 8 + 1
+                n = i % 8 + 1
+                # Table index from 0, this is math
+                xindex = offset + (i // 7) * 2
+                xdata = offset + (i // 7) * 2 + 1
+                y = metalines + i % 7
+                index = f"{m}.{n}" if numbering == "m.8" else f"{i+1}"
+                cell = None
+                # XXX: a better data structure to avoid traverse?
+                for d in data:
+                    if d["class"] == str(classn) and d["coord"] == coord and d["index"] == index:
+                        cell = d
                         break
-                    # XXX: a better data structure to avoid traverse?
-                    for d in data:
-                        if d["class"] == str(classn) and d["coord"] == coord and d["index"] == index:
-                            cell = d
-                            break
-                    if cell is None:
-                        raise IndexError(f"Not found:{classn}/{coord}/{index}")
-                    if m == 4:
-                        if n == 5:
-                            index = "国旗"
-                        if n == 6:
-                            index = "校旗"
-                    table.rows[n].cells[m*2-2].text = index
-                    tconversion = {
-                        "W": "白",
-                        "R": "红",
-                        "B": "蓝",
-                        "O": "橙",
-                        "Y": "黄",
-                        "K": "收"
-                    }
-                    try:
-                        table.rows[n].cells[m*2 -
-                                            1].text = tconversion[cell["color"]]
-                        table.rows[n].cells[m*2-1]._tc.get_or_add_tcPr().append(
-                            parse_xml(r'<w:shd {} w:fill="{}"/>'.format(
-                                nsdecls('w'), cell["colorcode"])))
-                    except KeyError:
-                        print(
-                            f"Warning: malformed color code `{cell['color']}' at {classn}/{coord}/{index}", file=sys.stderr)
+                else:
+                    raise LookupError(f"Not found:{classn}/{coord}/{index}")
+                table.cell(y, xindex).text = index
+                tconversion = {
+                    "W": "白",
+                    "R": "红",
+                    "B": "蓝",
+                    "O": "橙",
+                    "Y": "黄",
+                    "K": "收"
+                }
+                try:
+                    table.cell(y, xdata).text = tconversion[cell["color"]]
+                    # If no lyrics, color the data cell
+                    coloredcell = table.cell(y, xdata)
+                    if lyrics:
+                        table.cell(y, xdata).text += "," + lyricslst[i]
+                        # Else color the index cell
+                        coloredcell = table.cell(y, xindex)
+                    coloredcell._tc.get_or_add_tcPr().append(
+                        parse_xml(r'<w:shd {} w:fill="{}"/>'.format(
+                            nsdecls('w'), cell["colorcode"])))
+                except KeyError:
+                    print(
+                        f"Warning: malformed color code `{cell['color']}' at {classn}/{coord}/{index}", file=sys.stderr)
             # Add a empty line after every table and split every four into two pages
-            if person % 4 == 3:
+            if person % 8 == 7:
                 doc.add_page_break()
             else:
-                doc.add_paragraph()
+                p = doc.add_paragraph(text='')
+                p.paragraph_format.line_spacing = Cm(0.1)
     doc.save(outaddr)
 
 
@@ -214,8 +236,12 @@ def main():
         "-c", "--cache", help="Read the cache into a JSON file")
     parser.add_argument(
         "-o", "--only", help="The class number to generate, comma seperated[all]")
+    parser.add_argument("-a", "--analyzeonly",
+                        help="Do not generate, analyze only", action="store_true")
+    parser.add_argument("-l", "--lyrics", help="Lyrics json file")
+    # The m.8 is kept for historical reasons
     parser.add_argument("-n", "--numbering", help="The numbering scheme, `m.8' for the eight beats numbering and `int' for continuing numbers",
-                        choices=["m.8", "int"], default="m.8")
+                        choices=["m.8", "int"], default="int")
     parser.add_argument("pptfile", help="The input pptx file")
     args = parser.parse_args()
     if not args.cache:
@@ -226,8 +252,9 @@ def main():
     if args.save:
         json.dump([result, length], open(args.save, "w"))
     print("Reading done, generating...")
-    gendoc(result, length, args.numbering, args.pptfile +
-           ".docx", [int(c) for c in args.only.split(',')] if args.only else None)
+    if not args.analyzeonly:
+        gendoc(result, length, args.numbering, args.pptfile +
+               ".docx", [int(c) for c in args.only.split(',')] if args.only else None, args.lyrics)
 
 
 if __name__ == "__main__":
