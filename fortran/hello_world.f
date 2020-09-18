654b97f578c5596faaac6f0b152723830dc92261
Zhang Maiyun <myzhang1029@hotmail.com>
2020-09-19 00:13:00 +0800

    Add fortran program

---
+++ fortran/hello_world.f
@@ -0,0 +1,15 @@
+* My first Fortran program
+* Hello World 2020.9.18
+      PROGRAM HELLO_WORLD
+      IMPLICIT NONE
+      REAL :: A, B
+      INTEGER COUNTER
+      PRINT *, "Hello World!"
+      A = 42
+      COUNTER = 0
+42    B = TAN(A)
+      PRINT *, B
+      A = B
+      COUNTER = COUNTER + 1
+      GOTO 42
+      END PROGRAM HELLO_WORLD
