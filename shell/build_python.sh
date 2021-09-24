46c9024e8f6abda35abb2781b5918fb3d4ea6c1c
Zhang Maiyun <myzhang1029@hotmail.com>
2021-09-24 07:47:41 -0700

    (build_python) Update and fix Linux

--- shell/build_python.sh
+++ shell/build_python.sh
@@ -16,8 +16,8 @@ NCURSES_V="${NCURSES:-"6.2"}"
 ZLIB_V="${ZLIB_V:-"1.2.11"}"
 GDBM_V="${GDBM_V:-"1.20"}"
 XZ_V="${XZ_V:-"5.2.5"}"
-LIBRESSL_V="${LIBRESSL_V:-"3.3.3"}"
-PYTHON_V="${PYTHON_V:-"3.9.6"}"
+OPENSSL_V="${OPENSSL_V:-"1.1.1l"}"
+PYTHON_V="${PYTHON_V:-"3.9.7"}"
 
 # Prepare build environment
 mkdir -p "${PREFIX}"
@@ -29,34 +29,36 @@ if [ -f ".download_complete.marker" ]
 then
     echo "WARNING: Assuming download has completed, if not, remove $PWD/.download_complete.marker and continue." >&2
 else
-    mkdir -p .downloads || true
-    cd .downloads
-    "$DOWNLOAD" $DOWNFLAGS \
-        https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-"$MPDECIMAL_V".tar.gz \
-	    ftp://ftp.cwru.edu/pub/bash/readline-"$READLINE_V".tar.gz \
-	    https://sqlite.org/2021/sqlite-autoconf-"$SQLITE3_V".tar.gz \
-	    https://prdownloads.sourceforge.net/tcl/tcl"$TCL_V"-src.tar.gz \
-	    https://prdownloads.sourceforge.net/tcl/tk"$TCL_V"-src.tar.gz \
-        https://github.com/libffi/libffi/releases/download/v"$LIBFFI_V"/libffi-"$LIBFFI_V".tar.gz \
-	    ftp://ftp.gnu.org/gnu/ncurses/ncurses-"$NCURSES_V".tar.gz \
-	    https://www.zlib.net/zlib-"$ZLIB_V".tar.gz \
-	    ftp://ftp.gnu.org/gnu/gdbm/gdbm-"$GDBM_V".tar.gz \
-	    https://tukaani.org/xz/xz-"$XZ_V".tar.gz \
-	    https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-"$LIBRESSL_V".tar.gz \
-	    https://www.python.org/ftp/python/"$PYTHON_V"/Python-"$PYTHON_V".tgz
-    cd ..
+    mkdir -p build_downloads || true
+    (
+        cd build_downloads
+        # shellcheck disable=SC2086 # intended
+        "$DOWNLOAD" $DOWNFLAGS \
+            https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-"$MPDECIMAL_V".tar.gz \
+	        ftp://ftp.cwru.edu/pub/bash/readline-"$READLINE_V".tar.gz \
+	        https://sqlite.org/2021/sqlite-autoconf-"$SQLITE3_V".tar.gz \
+	        https://prdownloads.sourceforge.net/tcl/tcl"$TCL_V"-src.tar.gz \
+	        https://prdownloads.sourceforge.net/tcl/tk"$TCL_V"-src.tar.gz \
+            https://github.com/libffi/libffi/releases/download/v"$LIBFFI_V"/libffi-"$LIBFFI_V".tar.gz \
+	        ftp://ftp.gnu.org/gnu/ncurses/ncurses-"$NCURSES_V".tar.gz \
+	        https://www.zlib.net/zlib-"$ZLIB_V".tar.gz \
+	        ftp://ftp.gnu.org/gnu/gdbm/gdbm-"$GDBM_V".tar.gz \
+	        https://tukaani.org/xz/xz-"$XZ_V".tar.gz \
+            https://www.openssl.org/source/openssl-"$OPENSSL_V".tar.gz \
+	        https://www.python.org/ftp/python/"$PYTHON_V"/Python-"$PYTHON_V".tgz
+    )
     touch .download_complete.marker
 fi
 
 # Unpack sources
-mkdir -p .sources || true
-cd .sources
+mkdir -p build_sources || true
+cd build_sources
 
 if [ -f ".unpack_complete.marker" ]
 then
-    echo "WARNING: Assuming unpack has completed, if not, remove $PWD/.download_complete.marker and continue." >&2
+    echo "WARNING: Assuming unpack has completed, if not, remove $PWD/.unpack_complete.marker and continue." >&2
 else
-    for file in ../.downloads/*
+    for file in ../build_downloads/*
     do
 	    echo "Unpacking $file"
 	    tar zxvf "$file"
@@ -65,23 +67,32 @@ else
 fi
 
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib"
 
 # $1: Package name/path
 # $2: Path to configure relative to package root
 # $3: Arguments to configure
 build()
 {
-    if [ -f "$1/.build_complete.marker" ]
+    pkg="$1"
+    configure="$2"
+    shift 2
+    if [ -f "$pkg/.build_complete.marker" ]
     then
-        echo "WARNING: Assuming $1 has been built successfully, if not, remove $PWD/$1/.build_complete.marker and continue." >&2
+        echo "WARNING: Assuming $pkg has been built successfully, if not, remove $PWD/$pkg/.build_complete.marker and continue." >&2
     else
-        echo Building "$1"
-	    cd "$1"
-	    CFLAGS="-I$PREFIX/include $CFLAGS" LDFLAGS="-L$PREFIX/lib $LDFLAGS" "$2" --prefix="$PREFIX" $3
-	    make -j"$NPROC" install
-	    make distclean
-        touch .build_complete.marker
-        cd ..
+        echo Building "$pkg"
+        (
+	        cd "$pkg"
+	        CFLAGS="-I$PREFIX/include $CFLAGS" \
+	            CPPFLAGS="-I$PREFIX/include $CPPFLAGS" \
+                LDFLAGS="-L$PREFIX/lib $LDFLAGS" \
+                "$configure" --prefix="$PREFIX" "$@"
+	        make -j"$NPROC"
+	        make install
+	        make distclean
+            touch .build_complete.marker
+        )
     fi
 }
 
@@ -98,14 +109,16 @@ for package in zlib-"$ZLIB_V" \
     readline-"$READLINE_V" \
     gdbm-"$GDBM_V" \
     sqlite-autoconf-"$SQLITE3_V" \
-    libressl-"$LIBRESSL_V" \
     xz-"$XZ_V" \
     libffi-"$LIBFFI_V"
 do
     build "$package" "./configure"
 done
 
-build "Python-$PYTHON_V" "./configure" --enable-optimizations
+build openssl-"$OPENSSL_V" "./config"
+
+build "Python-$PYTHON_V" "./configure" --enable-ipv6 --enable-optimizations --with-openssl="$PREFIX" --with-lto --with-system-libmpdec --with-system-ffi
 
 echo "Your Python $PYTHON_V is available at $PREFIX"
+echo "If you are using Linux, you may need to set LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$PREFIX/lib\" apart from \$PATH"
 exit 0
