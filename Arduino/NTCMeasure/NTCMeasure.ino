fa1e8812f9f6b7ca6bc1cf70fc92f7637620e9cb
Zhang Maiyun <myzhang1029@hotmail.com>
2022-01-20 08:21:03 -0800

    (NTCMeasure) Simplify math

--- Arduino/NTCMeasure/NTCMeasure.ino
+++ Arduino/NTCMeasure/NTCMeasure.ino
@@ -1,18 +1,16 @@
 // VISHAY NTC 2381-640-55103
 // Datasheet: http://connect.iobridge.com/wp-content/uploads/2012/02/thermistor_2381-640-55103.pdf
 const int sensorPin = A1;
-// Analog ref
-const float VAref = 5.0; // Volts
 // NTC base resistance
 const float R0 = 1e4; // Ohm \pm 1%
 // Temperature corresponding to R0
 const float T0 = 25.0 + 273.15; // Kelvin
 // divider resistance
-const float R = 1e4; // Ohm \pm 1%
+const float Rdiv = 1e4; // Ohm \pm 1%
 const float BETA = 3977; // Kelvin \pm 0.75%
 
 // For pin out like this:
-// Aref - NTC - R - GND
+// Aref - NTC - Rdiv - GND
 //            | sensorPin
 
 void setup() {
@@ -21,8 +19,9 @@ void setup() {
 
 void loop() {
   int sensorValue = analogRead(sensorPin); // \pm 0.5/sensorValue
-  float VR = (VAref / 1024.00) * sensorValue;
-  float NTC = R * (VAref - VR) / VR;
+  // VRdiv = (VAref / 1024.0) * sensorValue
+  // NTC = Rdiv * (VAref - VRdiv) / VRdiv
+  float NTC = Rdiv * (1024.0 / sensorValue - 1);
   // R = R0 * exp(beta/T - beta/T0)
   // ln(R/R0) + beta/T0 = beta/T
   float T = BETA / (log(NTC/R0) + BETA/T0);
