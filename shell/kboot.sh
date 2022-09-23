0d4ed958efb2c2ebfa7f00ec3ccf29838f5ac60d
Zhang Maiyun <me@maiyun.me>
2022-09-22 21:26:41 -0700

    (kboot.sh) Add

---
+++ shell/kboot.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# kboot
+# kboot [kernel] [params] [append params ...]
+
+# The first arg (if any) specifies the kernel, where a '-' means use the current kernel.
+# If the 2nd arg is '-' or is omitted, then the existing kernel parameters are appended.
+# Any remaining args are also appended to the kernel parameters.
+
+if [ $# -eq 0 ]; then
+    reuse=--reuse-cmdline
+elif ! [ "$1" = "-" ]; then
+    kernel="$1"
+    shift
+fi
+if [ $# -eq 0 ]; then
+    reuse=--reuse-cmdline
+elif [ "$1" = "-" ]; then
+    reuse=--reuse-cmdline
+    shift
+fi
+kernel="${kernel:-$(uname -r)}"
+kargs="/boot/vmlinuz-$kernel --initrd=/boot/initrd.img-$kernel"
+
+kexec -l -t bzImage $kargs $reuse --append="$*" && systemctl kexec
