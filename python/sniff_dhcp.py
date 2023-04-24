3700379a38898d1deb8c76858698737af983dd0a
Zhang Maiyun <me@maiyun.me>
2023-04-23 23:03:24 -0700

    (sniff_dhcp.py) Add a lot of new processing power

--- python/sniff_dhcp.py
+++ python/sniff_dhcp.py
@@ -25,47 +25,40 @@ import json
 import queue
 import sys
 import time
-from dataclasses import dataclass
-from typing import Dict, List, Optional, Tuple
+from collections import defaultdict
+from dataclasses import dataclass, field
+from ipaddress import IPv4Address, IPv6Address, ip_address, ip_network
+from typing import Optional, TypeVar
 
 import scapy
-from scapy.all import DHCP, DNS, DNSRR, UDP, Ether, LLMNRResponse, getmacbyip, getmacbyip6
+from scapy.all import (DHCP, DNS, DNSRR, UDP, Ether, LLMNRResponse, getmacbyip,
+                       getmacbyip6)
 
 import manage_hosts
 
-class MissingData(Exception):
-    """Missing data."""
+HostSelf = TypeVar("HostSelf", bound="Host")
 
-class NoAddressError(MissingData):
-    """Raised when no address is found."""
 
-class NoMACError(MissingData):
+class NoMACError(Exception):
     """Raised when no MAC is found."""
+
 
 @dataclass
 class Host:
     """A host."""
-    # Source of the information
-    source: str
-    ipv4: Optional[str] = None
-    ipv6: Optional[str] = None
+    ip: set[IPv4Address | IPv6Address] = field(default_factory=set)
     # Any form of hostname
     hostname: Optional[str] = None
-    # mDNS hostname
+    # mDNS hostname (without .local.)
     dns_hostname: Optional[str] = None
     # MAC address
     mac: Optional[str] = None
+    # Comments
+    comments: set[str] = field(default_factory=set)
 
-    def to_items(self) -> List[Tuple[str, str, str, str]]:
+    def to_items(self: HostSelf) -> list[tuple[IPv4Address | IPv6Address, str, str, set[str]]]:
         """Convert to a list of items."""
         messages = []
-        if not self.ipv4 and not self.ipv6:
-            raise NoAddressError(f"No IP address for {self}")
-        if not self.mac:
-            # Try to resolve MAC address again
-            self.fill_mac_from_ip()
-        if not self.mac:
-            raise NoMACError(f"No MAC address for {self}")
         if self.hostname:
             hostnames = [self.hostname]
             if self.dns_hostname:
@@ -74,48 +67,62 @@ class Host:
             hostnames = [self.dns_hostname]
         else:
             hostnames = ["<unknown>"]
-        if self.ipv4:
+        if not self.mac:
+            # Try to resolve MAC address again
+            self.fill_mac_from_ip()
+        if not self.mac and self.ip:
+            raise NoMACError(f"No MAC address for {self}")
+        for addr in self.ip:
             for hostname in hostnames:
-                messages.append((self.ipv4, self.mac, hostname, "Sniff-Source: " + self.source))
-        if self.ipv6:
-            for hostname in hostnames:
-                messages.append((self.ipv6, self.mac, hostname, "Sniff-Source: " + self.source))
+                messages.append((addr, self.mac, hostname, self.comments))
         return messages
 
-    def to_mh_tsv(self) -> str:
-        """Convert to manage_hosts TSV format."""
-        return '\n'.join(f"{i[0]}\t{i[1]}\t{i[2]} # {i[3]}" for i in self.to_items())
+    def with_source(self: HostSelf, source: str) -> HostSelf:
+        """Add source."""
+        self.comments.add(f"Sniff-Source: {source}")
+        return self
 
-    def ipv4_from_arpa(self, arpa_form: bytes) -> None:
-        """Set IPv4 address from arpa form."""
+    def with_ip(self: HostSelf, addr: Optional[IPv4Address | IPv6Address]) -> HostSelf:
+        """Add an IP address."""
+        if addr:
+            self.ip.add(addr)
+        return self
+
+    def fill_mac_from_ip(self: HostSelf) -> HostSelf:
+        """Resolve MAC address from IP address."""
+        for addr in self.ip:
+            if self.mac:
+                break
+            if isinstance(addr, IPv4Address):
+                self.mac = getmacbyip(addr.compressed)
+            elif isinstance(addr, IPv6Address) and addr not in ip_network("fc00::/7"):
+                self.mac = getmacbyip6(addr.compressed)
+        return self
+
+
+def arpa_to_ip(arpa_form: bytes) -> IPv4Address | IPv6Address:
+    """Convert from arpa form to IP address."""
+    if arpa_form.endswith(b".in-addr.arpa."):
         ip = arpa_form.removesuffix(b".in-addr.arpa.").decode("utf-8")
         # Convert from reverse pointer format to normal IP
-        self.ipv4 = ".".join(reversed(ip.split(".")))
-
-    def ipv6_from_arpa(self, arpa_form: bytes) -> None:
-        """Set IPv6 address from arpa form."""
+        ip = '.'.join(reversed(ip.split(".")))
+        return IPv4Address(ip)
+    elif arpa_form.endswith(b".ip6.arpa."):
         ip = arpa_form.removesuffix(b".ip6.arpa.").decode("utf-8")
         # Convert from reverse pointer format to normal IP
         rev_segs = reversed(ip.split("."))
-        words = ("".join(segs) for segs in zip(*[iter(rev_segs)] * 4, strict=True))
-        self.ipv6 = ':'.join(words).lower()
-
-    def fill_mac_from_ip(self) -> None:
-        """Resolve MAC address from IP address."""
-        mac = None
-        if self.ipv4:
-            mac = getmacbyip(self.ipv4)
-        if not mac and self.ipv6:
-            mac = getmacbyip6(self.ipv6)
-        if mac:
-            self.mac = mac
+        words = ("".join(segs)
+                 for segs in zip(*[iter(rev_segs)] * 4, strict=True))
+        ip = ":".join(words)
+        return IPv6Address(ip)
+    else:
+        raise ValueError(
+            f"{arpa_form!r} does not appear to be a reverse DNS pointer")
 
 
-def digest_dhcp_packet(frame) -> Optional[Host]:
+def digest_dhcp_packet(frame) -> Host:
     """Process frames containing DHCP payloads."""
-    mac = None
-    hostname = None
-    ip = None
+    result = Host().with_source("DHCP")
     for opt in frame[DHCP].options:
         if opt[0] == "end":
             break
@@ -123,59 +130,132 @@ def digest_dhcp_packet(frame) -> Optional[Host]:
         value = opt[1]
         if k == "message-type" and value in (1, 3):
             # DISCOVER and REQUEST
-            mac = frame[Ether].src
+            result.mac = frame[Ether].src
         if k == "hostname":
-            hostname = value.decode("utf-8")
+            result.hostname = value.decode("utf-8")
         if k == "requested_addr":
-            ip = value
-    if all((ip, hostname, mac)):
-        return Host(ipv4=ip, hostname=hostname, mac=mac, source="DHCP")
-    return None
+            result.with_ip(ip_address(value))
+    return result
 
 
-def digest_dnsrr_ptr_airdrop_like(dnsrr: DNSRR, arcount: int, arlist) -> Optional[Host]:
-    """Extract information from PTR records like *._companion-link._tcp.local.
-    
-    Returns `None` if the PTR record is not used.
-    """
+@dataclass
+class ExtractedDNSAnswer:
+    """Information extracted from a single mDNS/LLMNR answer."""
     # These are the ones I have seen
-    servnames = (b"_googlecast._tcp.local.", b"_companion-link._tcp.local.")
-    # Things we want to extract
-    data = Host(source="mDNS PTR and SRV")
-    # DNS-hostname to IP (bytes to str)
-    hosts4 = {}
-    hosts6 = {}
-    for servname in servnames:
-        # _X._proto.local. N IN PTR <hostname>._X._proto.local.
-        if dnsrr.rdata.endswith(servname) and dnsrr.rrname == servname:
-            hostname_servname = dnsrr.rdata
-            data.hostname = hostname_servname.removesuffix(b"." + servname).decode("utf-8")
-            dns_hostname = b""
-            # See if there is an additional section with the IP and the DNS hostname
-            for i in range(arcount):
-                ar = arlist[i]
-                # A or AAAA: extract the relationship to `hosts`
-                if ar.type == 1:
-                    hosts4[ar.rrname] = ar.rdata
-                elif ar.type == 28:
-                    hosts6[ar.rrname] = ar.rdata
-                # SRV: find the corresponding DNS hostname and save all
-                elif ar.type == 33 and ar.rrname == hostname_servname:
-                    dns_hostname = ar.target
-            data.ipv4 = hosts4.get(dns_hostname)
-            data.ipv6 = hosts6.get(dns_hostname)
-            data.dns_hostname = dns_hostname.decode("utf-8").removesuffix(".local.")
-            data.fill_mac_from_ip()
-            return data
-    return None
+    AIRPLAY_LIKE_SERVS = (
+        b"_airplay._tcp.local.",
+        b"_companion-link._tcp.local.",
+        b"_device-info._tcp.local.",
+        b"_googlecast._tcp.local.",
+        b"_rdlink._tcp.local.",
+    )
+    # DNS-hostname (with .local.) to IP (bytes to set of IPv4/IPv6 addresses)
+    # From A/AAAA records
+    dns_to_ip: defaultdict[bytes, set[IPv4Address | IPv6Address]] = field(
+        default_factory=lambda: defaultdict(set))
+    # (hostname, servname) to DNS-hostname (with .local.)
+    # From SRV records
+    host_to_dns: defaultdict[tuple[bytes, bytes],
+                             bytes] = field(default_factory=lambda: defaultdict(bytes))
+    # hostname to other information
+    # From TXT records
+    host_to_comments: defaultdict[bytes, set[str]] = field(
+        default_factory=lambda: defaultdict(set))
+
+    def populate_from_a_or_aaaa(self, rrname: bytes, rdata: str) -> None:
+        """Populate from A or AAAA record:
+
+        rrname: DNS hostname
+        rdata: IP address
+        """
+        self.dns_to_ip[rrname].add(ip_address(rdata))
+
+    def populate_from_srv(self, rrname: bytes, target: bytes) -> None:
+        """Populate from SRV record:
+
+        rrname: hostname.servname
+        target: DNS hostname
+        """
+        for servname in self.AIRPLAY_LIKE_SERVS:
+            if rrname.endswith(servname):
+                hostname = rrname.removesuffix(b"." + servname)
+                self.host_to_dns[(hostname, servname)] = target
+                break
+        else:
+            print(f"Unknown SRV record: {rrname} -> {target}")
+
+    def populate_from_ptr(self, rrname: bytes, rdata: bytes) -> None:
+        """Populate from PTR record:
+
+        1. rrname: servname; rdata: hostname.servname
+        2. rrname: IP in reverse pointer format; rdata: DNS hostname
+        """
+        if rrname in self.AIRPLAY_LIKE_SERVS:
+            # Case 1: Do nothing because they don't provide any useful information
+            pass
+        elif rrname.endswith(b".in-addr.arpa.") or rrname.endswith(b".ip6.arpa."):
+            # Case 2
+            self.dns_to_ip[rdata].add(arpa_to_ip(rrname))
+        else:
+            print(f"Unknown PTR record: {rrname} -> {rdata}")
+
+    def populate_from_txt(self, rrname: bytes, rdata: list[bytes]) -> None:
+        """Populate from TXT record:
+
+        rrname: hostname.servname
+        rdata: <some data>
+        """
+        for servname in self.AIRPLAY_LIKE_SERVS:
+            if rrname.endswith(servname):
+                hostname = rrname.removesuffix(b"." + servname)
+                for data in rdata:
+                    if data.startswith(b"model="):
+                        model = data.split(b"=", 1)[1].decode("utf-8")
+                        self.host_to_comments[hostname].add(f"Model: {model}")
+                break
+
+    def extract_hosts(self) -> list[Host]:
+        """Extract hosts from the information and discard this object."""
+        hosts: list[Host] = []
+        # First exhaust host_to_dns and consume related information
+        while self.host_to_dns:
+            (hostname, servname), dnsname = self.host_to_dns.popitem()
+            if dnsname in self.dns_to_ip:
+                ip = self.dns_to_ip.pop(dnsname)
+                comments = self.host_to_comments.pop(
+                    hostname, set())
+                host = Host(
+                    comments=comments,
+                    dns_hostname=dnsname.removesuffix(
+                        b".local.").decode("utf-8"),
+                    hostname=hostname.decode("utf-8"),
+                    ip=ip
+                ).with_source("mDNS PTR and SRV").fill_mac_from_ip()
+                hosts.append(host)
+        # Then exhaust dns_to_ip and consume related information
+        while self.dns_to_ip:
+            dnsname, ip = self.dns_to_ip.popitem()
+            if not ip:
+                continue
+            host = Host(
+                dns_hostname=dnsname.removesuffix(b".local.").decode("utf-8"),
+                ip=ip
+            ).fill_mac_from_ip()
+            if isinstance(next(iter(ip)), IPv4Address):
+                host.with_source("mDNS A")
+            else:
+                host.with_source("mDNS AAAA")
+            hosts.append(host)
+        # Outstanding host_to_comments are discarded
+        return hosts
 
 
-def digest_mdns_or_llmnr_packet(frame) -> Optional[Host]:
-    """Process frames containing mDNS or LLMNR payloads."""
+def digest_mdns_or_llmnr_packet(frame) -> list[Host]:
+    """Process a single packet containing mDNS or LLMNR payloads."""
     udp = frame[UDP]
     if udp.sport not in (5353, 5355) and udp.dport not in (5353, 5355):
         # Not mDNS nor LLMNR
-        return None
+        return []
     if DNS in frame:
         answers = frame[DNS].an
         ars = frame[DNS].ar
@@ -187,52 +267,49 @@ def digest_mdns_or_llmnr_packet(frame) -> Optional[Host]:
         ancount = frame[LLMNRResponse].ancount
         arcount = frame[LLMNRResponse].arcount
     else:
-        return None
+        return []
+    data = ExtractedDNSAnswer()
     for i in range(ancount):
         dnsrr = answers[i]
-        # A records
-        if dnsrr.type == 1:
-            hostname = dnsrr.rrname.decode("utf-8").removesuffix(".local.")
-            data = Host(ipv4=dnsrr.rdata, hostname=hostname, source="mDNS A")
-            data.fill_mac_from_ip()
-            return data
-        # AAAA records
-        elif dnsrr.type == 28:
-            hostname = dnsrr.rrname.decode("utf-8").removesuffix(".local.")
-            data = Host(ipv6=dnsrr.rdata, hostname=hostname, source="mDNS AAAA")
-            data.fill_mac_from_ip()
-            return data
+        # A/AAAA records
+        if dnsrr.type == 1 or dnsrr.type == 28:
+            data.populate_from_a_or_aaaa(dnsrr.rrname, dnsrr.rdata)
         # PTR records
         elif dnsrr.type == 12:
-            maybe_data = digest_dnsrr_ptr_airdrop_like(dnsrr, arcount, ars)
-            if maybe_data:
-                return maybe_data
-            data = Host(source="mDNS PTR")
-            # Then we only care about arpa. types
-            if dnsrr.rrname.endswith(b".in-addr.arpa."):
-                data.ipv4_from_arpa(dnsrr.rrname)
-                data.fill_mac_from_ip()
-                data.hostname = dnsrr.rdata.removesuffix(
-                    b".local.").decode("utf-8")
-                return data
-            elif dnsrr.rrname.endswith(b".ip6.arpa."):
-                data.ipv6_from_arpa(dnsrr.rrname)
-                data.fill_mac_from_ip()
-                data.hostname = dnsrr.rdata.removesuffix(
-                    b".local.").decode("utf-8")
-                return data
-    return None
+            data.populate_from_ptr(dnsrr.rrname, dnsrr.rdata)
+        # SRV records
+        elif dnsrr.type == 33:
+            data.populate_from_srv(dnsrr.rrname, dnsrr.target)
+        # TXT records
+        elif dnsrr.type == 16:
+            data.populate_from_txt(dnsrr.rrname, dnsrr.rdata)
+    for i in range(arcount):
+        dnsrr = ars[i]
+        # A/AAAA records
+        if dnsrr.type == 1 or dnsrr.type == 28:
+            data.populate_from_a_or_aaaa(dnsrr.rrname, dnsrr.rdata)
+        # PTR records
+        elif dnsrr.type == 12:
+            data.populate_from_ptr(dnsrr.rrname, dnsrr.rdata)
+        # SRV records
+        elif dnsrr.type == 33:
+            data.populate_from_srv(dnsrr.rrname, dnsrr.target)
+        # TXT records
+        elif dnsrr.type == 16:
+            data.populate_from_txt(dnsrr.rrname, dnsrr.rdata)
+    return data.extract_hosts()
 
 
 # Queue for lines to be printed
-results: queue.SimpleQueue[Host] = queue.SimpleQueue()
+results: queue.SimpleQueue[list[Host]] = queue.SimpleQueue()
+
 
 def digest_packet(frame) -> None:
     """Process captured frames."""
     if DHCP in frame and (host := digest_dhcp_packet(frame)):
-        results.put(host)
-    if (DNS in frame or LLMNRResponse in frame) and (host := digest_mdns_or_llmnr_packet(frame)):
-        results.put(host)
+        results.put([host])
+    if (DNS in frame or LLMNRResponse in frame) and (hosts := digest_mdns_or_llmnr_packet(frame)):
+        results.put(hosts)
 
 
 sniffer = scapy.sendrecv.AsyncSniffer(
@@ -246,31 +323,30 @@ db = manage_hosts.MacDatabase("stagingscan.json")
 while True:
     # Exhaust the buffer
     try:
-        host = results.get_nowait()
-        try:
-            # Save the host
-            for line in host.to_items():
-                # Filter out bogus
-                if line[0] == "::" or line[0] == "0.0.0.0":
-                    break
-                if line[1] == "00:00:00:00:00:00":
-                    break
-                if line[1] == "ff:ff:ff:ff:ff:ff":
-                    break
-                if line[0].startswith("169.254."):
-                    break
-                # This one produces a lot of unwanted noise
-                if line[0].startswith("fd"):
-                    break
-            else:
-                # Not rejected, save
-                # Print the line
-                print(host.to_mh_tsv())
-                for line in host.to_items():
-                    db.add(line[0], line[2], line[1], (line[3],))
-        except MissingData as err:
-            print(err, file=sys.stderr)
-    except queue.Empty:
-        print("No new hosts")
+        for host in results.get_nowait():
+            print("Got packet")
+            try:
+                items = host.to_items()
+                # Save the host
+                for line in items:
+                    # Filter out bogus
+                    if line[0].is_unspecified:
+                        break
+                    if line[1] == "00:00:00:00:00:00":
+                        break
+                    if line[1] == "ff:ff:ff:ff:ff:ff":
+                        break
+                    if line[0] in ip_network("169.254.0.0/16"):
+                        break
+                else:
+                    # Not rejected, save
+                    # Print the line
+                    print(
+                        *(f"{i[0]}\t{i[1]}\t{i[2]} # {' '.join(i[3])}" for i in items), sep="\n")
+                    for line in items:
+                        db.add(line[0], line[2], line[1], line[3])
+            except NoMACError as err:
+                print(err, file=sys.stderr)
         db.save()
+    except queue.Empty:
         time.sleep(1)
