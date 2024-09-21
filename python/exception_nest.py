f19b9edd999f6b815b12df65782c6da051092cdf
Zhang Maiyun <me@maiyun.me>
2024-09-20 17:17:14 -0700

    (exception_nest) Some background

--- python/exception_nest.py
+++ python/exception_nest.py
@@ -4,7 +4,7 @@ Generates exceptionally useless code :-P
 
 also
 the interpreter crashes with
-CPython 3,7, 3.8
+CPython 3.7, 3.8
 iMac - macOS Catalina 10.15.3
 Raspberry Pi 4
 
@@ -13,6 +13,8 @@ depth:
    11..20: Fatal Python error: XXX block stack overflow (Abort)
    21..99: SyntaxError: too many statically nested blocks
    >100:   IndentationError: too many levels of indentation
+
+Reported [BPO-39934](https://bugs.python.org/issue39934) -> cpython#84115.
 
 in PyPy 7.3.0 the maximum for a is 2451 and a RecursionError is raised
 Running in ipython doesn't cause the Fatal Error
