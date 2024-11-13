d6065a202062a6105fd814d4d7661a5491f0c200
Zhang Maiyun <me@maiyun.me>
2024-11-12 17:36:19 -0800

    (init.m) New utilities

--- Wolfram/init.m
+++ Wolfram/init.m
@@ -3,13 +3,14 @@
 (* Always prefer metric units *)
 $UnitSystem = "Metric"
 
-(* Statistics Tools *)
+(* Statistics *)
 
 (* Derivative of `Abs` *)
 Abs'[x_] := Sign[x] /; x != 0
 
 (* Normal CDF *)
 Phi[x_] := Erfc[-(x/Sqrt[2])]/2
+Attributes[Phi] = {Listable, NumericFunction}
 
 (* Compute relative error *)
 RelativeError[x_Around] := x["Uncertainty"] / x["Value"]
@@ -23,25 +24,46 @@ MeanAround2[data : {___Around}] := With[
 ]
 MeanAround2[data : {___Quantity}] := Quantity[MeanAround2@QuantityMagnitude@data, QuantityUnit@First@data]
 
+(* Engineering *)
+
+(* The hypotenuse of a right-angle triangle *)
+Hypot[a_, b_] := Sqrt[a^2 + b^2]
+Attributes[Hypot] = {Listable, NumericFunction}
+
 (* Decibel conversion *)
-dBu[dbu_] := First@SolveValues[
-    20 Log[10, x/Quantity[Sqrt[3/5], "Volts"]] == dbu,
-    x,
-    Reals
-]
-dBV[dbv_] := First@SolveValues[
+dBm[dBm_] := Quantity[10^(dBm/10), "Milliwatts"]
+(* dBV[dbv_] := First@SolveValues[
     20 Log[10, x/Quantity[1, "Volts"]] == dbv,
     x,
     Reals
-]
+] *)
+dBV[dbv_] := Quantity[10^(dbv/20), "Volts"]
+(* dBu[dbu_] := First@SolveValues[
+    20 Log[10, x/Quantity[Sqrt[3/5], "Volts"]] == dbu,
+    x,
+    Reals
+] *)
+dBu[dbu_] := Quantity[10^(dbu/20) Sqrt[3/5], "Volts"]
 
-(* Fourier analysis tools *)
+(* Irregular-Time Discrete Fourier Transform *)
 IDFT[(\[Omega]_)?(NumericQ[#] || QuantityQ[#] &), times_, vals_] :=
  Mean[vals E^(-I \[Omega] times)]
+(* Convert voltage FT to dB *)
 VFT2dB[x_] := 20 Log10@Abs[x]
+(* Engineering-convention Fourier Transform *)
+EFourierTransform[args___] := FourierTransform[args, FourierParameters -> {1, -1}]
 
-(* Make an augmented matrix *)
-AugmentedMatrix[mat_, vec_] := Transpose@Append[Transpose[mat], vec]
+(* The rect function *)
+Rect[x_] := Piecewise[{
+    {1, -(1/2) < x < 1/2},
+    {1/2, Abs[x] == 1/2},
+    {0, True}
+}]
+Attributes[Rect] = {Listable, NumericFunction}
+
+(* Engineering sinc function *)
+ESinc[x_] := Sinc[Pi x]
+Attributes[ESinc] = {Listable, NumericFunction}
 
 (* The multivariate Legendre transformation *)
 LegendreTransform[f_, v_, p_] := Module[
@@ -60,3 +82,15 @@ LegendreTransform[f_, v_, p_] := Module[
         ]
     ]
 ]
+
+(* Other miscellaneous things *)
+
+(* Make an augmented matrix *)
+AugmentedMatrix[mat_, vec_] := Transpose@Append[Transpose[mat], vec]
+
+(* Display the variable name when setting a value *)
+ESet[var_, value_] := (
+    Echo[ToString[HoldForm[var]] ~~ " is " ~~ ToString[value]];
+    var = value;
+)
+Attributes[ESet] = {HoldFirst, SequenceHold};
